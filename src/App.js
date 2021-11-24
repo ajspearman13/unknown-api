ea7955ffed46c6329ac02480b308bb385121052f
Allan Spearman <allanspearman@allans-air.attlocal.net>
2021-11-24 11:26:54 -0500

    created react app and accessed api site, need to make props for Pokemon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+//<script src="https://npmcdn.com/axios/dist/axios.min.js"></script>
+
+const site = 'https://pokeapi.co/api/v2/pokemon/?limit=151'
+
+function Pokemon(props){
+
+  return 
+}
 
 function App() {
+  const [poke, setPoke] = useState('')
+  const [type, setType] = useState("")
+ 
+  const getSiteData = () =>{
+    // Make a request for a user with a given ID
+    axios.get(site) 
+      .then(response => {
+        // handle success
+        //setPoke(response.data.name)
+        setPoke(response.data.results[5].url )
+        
+      })      
+  }
+  
+  function next() {
+    axios.get(poke) 
+      .then(response => {
+        // handle success
+        //setPoke(response.data.name)
+        setType(response.data.name)
+        
+      })      
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokedex</h1>
+        <button onClick={getSiteData}> here </button>
+        <button onClick={next}> next </button>
+         
+         <ul> {type}</ul>
+         
+        
     </div>
+
   );
 }
+
 
 export default App;

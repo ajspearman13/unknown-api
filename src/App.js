997ca6c3e95ab741085c7bf23981d758d67de947
Allan Spearman <allanspearman@allans-air.attlocal.net>
2021-12-01 11:02:26 -0500

    turn link to json

--- src/App.js
+++ src/App.js
@@ -1,54 +1,68 @@
-import axios from 'axios';
+
 import './App.css';
-import React, { useState, useEffect } from 'react';
-//<script src="https://npmcdn.com/axios/dist/axios.min.js"></script>
 
-const site = 'https://pokeapi.co/api/v2/pokemon/?limit=151'
+import React, { useState , useEffect} from 'react';
+import Pokedex  from './Pokedex';
 
-function Pokemon(props){
+import axios from 'axios';
+const initialSite = "https://pokeapi.co/api/v2/pokemon/?limit=5"
 
-  return 
-}
+
 
 function App() {
-  const [poke, setPoke] = useState('')
-  const [type, setType] = useState("")
- 
-  const getSiteData = () =>{
-    // Make a request for a user with a given ID
-    axios.get(site) 
-      .then(response => {
-        // handle success
-        //setPoke(response.data.name)
-        setPoke(response.data.results[5].url )
-        
-      })      
-  }
+  const [siteArr, setSiteArr] = useState([])
+  const [nameArr, setNameArr] = useState("")
+  const [page, setPage] = useState(initialSite)
   
-  function next() {
-    axios.get(poke) 
-      .then(response => {
-        // handle success
-        //setPoke(response.data.name)
-        setType(response.data.name)
-        
-      })      
+  function nextPage(){
+    
+    axios.get(page).then(res =>{
+      setPage(res.data.next)
+    }) 
   }
 
+  function previousPage(){
+    axios.get(page).then(res =>{
+      setPage(res.data.previous)
+    }) 
+}
+
+   
+  
+    // Make a request for a user with a given ID
+    useEffect(()=> { 
+      axios.get(page) 
+            .then(response => {
+              // handle success
+              //setPoke(response.data.name)
+            setSiteArr(response.data.results.map(x =>  x.url) )
+            setNameArr(response.data.results.map(x =>  x.name) )
+              }) 
+            .catch((err) => {
+              console.error(err)
+            })   
+     }, [page])        
+  
+  
 
   return (
+                                  //Each poke site is an array with each url
     <div className="App">
       <h1>Pokedex</h1>
-        <button onClick={getSiteData}> here </button>
-        <button onClick={next}> next </button>
-         
-         <ul> {type}</ul>
+      <button onClick={previousPage} > prev </button>
+        <button onClick={nextPage} > next </button>
+            
+        
+        
+         <Pokedex arr={siteArr} name={nameArr} /> 
+        
          
         
     </div>
 
   );
 }
+
 
 
 export default App;
---
+++ src/Pokedex.js
@@ -0,0 +1,72 @@
+import App from "./App"
+import axios from 'axios';
+import React, { useState , useEffect} from 'react';
+
+
+
+
+function Pokedex(props){
+  
+  const links = props.arr.map(x => <Pokemon key={x} link={x}/>) 
+  
+ // console.log(list)
+  
+  return(
+      <div >
+       {links}
+      </div>
+  )
+}
+
+
+
+function Pokemon(props){
+ const [name, setName] = useState('')
+ const [type, setType] = useState('')
+ const [moves1, setMoves1] = useState('')
+ const [moves2, setMoves2] = useState('')
+ const [moves3, setMoves3] = useState('')
+ const [moves4, setMoves4] = useState('')
+ const [pic, setPic] = useState('')
+ const [baseEx, setBaseEx] = useState('')
+ const [hp, setHp] = useState('')
+
+  useEffect(()=> { 
+  axios.get(props.link) 
+        .then(res =>{
+         
+         setName(res.data.name.toLocaleUpperCase())
+         setMoves1(res.data.moves[  Math.floor((Math.random() * (res.data.moves.length) ))  ].move.name)
+         setMoves2(res.data.moves[  Math.floor((Math.random() * (res.data.moves.length) ))  ].move.name)
+         setMoves3(res.data.moves[  Math.floor((Math.random() * (res.data.moves.length) ))  ].move.name)
+         setMoves4(res.data.moves[  Math.floor((Math.random() * (res.data.moves.length) ))  ].move.name)
+         setType(res.data.types.map(x => x.type.name +" " ))
+          //setMoves(res.data.moves.map(x => x.move.name).sort().map(x => <li>{x}</li>)    )
+         setPic(res.data.sprites['front_default'])
+         setBaseEx(res.data['base_experience'])
+         setHp(res.data.stats.map(x => {
+         return <li>{x.stat.name }  {x["base_stat"]}</li> 
+         }))
+        })
+        .catch((err) => {
+          console.error(err)
+        })  
+  }, [props.link]) 
+
+
+  return(
+    <div  key={name}>
+      <img src={pic} alt={name}/>
+     <p>{name}<br/>{type}<br/> EXP {baseEx} <br/> {moves1}  {moves2}  {moves3}  {moves4}<br/></p>
+     <ul>
+       {hp}
+     </ul>
+
+
+    </div>
+    
+  )
+}
+  
+  export  default Pokedex;
+  
